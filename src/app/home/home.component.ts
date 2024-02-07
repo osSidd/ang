fc551ff8d9c8a224f70ad190169c8aa53b37a64b
Osama <107386980+osSidd@users.noreply.github.com>
2024-02-07 14:14:09 +0530

    property binding and interpolation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports:[
+    HousingLocationComponent,
+  ],
   template: `
   <section>
     <form>
@@ -10,9 +15,24 @@ import { Component } from '@angular/core';
       <button class="primary" type="button">Search</button>
     </form>
   </section>
+  <section class="results">
+    <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+  </section>
 `,
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  readonly baseUrl = 'http://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: HousingLocation = {
+    id:999,
+    name: 'Test Home',
+    city: 'Test City',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  }
 }
